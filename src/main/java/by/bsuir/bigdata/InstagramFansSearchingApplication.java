0b197a09afc88038c95b3f7d08774762b61138bd
Vladislav Dektiarev <Vladislav_Dektiarev@epam.com>
2018-04-03 14:41:36 +0300

    Add spring boot application class

---
+++ src/main/java/by/bsuir/bigdata/InstagramFansSearchingApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramFansSearchingApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(InstagramFansSearchingApplication.class, args);
+    }
+}
