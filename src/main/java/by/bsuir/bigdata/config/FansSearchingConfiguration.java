f7953602b42abb2229988b9096fdf42ead9986c3
Vladislav Dektiarev <eclipter97@gmail.com>
2018-04-03 23:39:12 +0300

    Add http client configuration

---
+++ src/main/java/by/bsuir/bigdata/config/FansSearchingConfiguration.java
@@ -0,0 +1,26 @@
+package by.bsuir.bigdata.config;
+
+import me.postaddict.instagram.scraper.cookie.CookieHashSet;
+import me.postaddict.instagram.scraper.cookie.DefaultCookieJar;
+import me.postaddict.instagram.scraper.interceptor.ErrorInterceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FansSearchingConfiguration {
+
+    @Bean
+    public OkHttpClient okHttpClient()
+    {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        return new OkHttpClient.Builder()
+                .addNetworkInterceptor(loggingInterceptor)
+                .addInterceptor(new ErrorInterceptor())
+                .cookieJar(new DefaultCookieJar(new CookieHashSet()))
+                .build();
+    }
+}
