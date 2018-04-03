548a452623f54b72280c8c5741959520f8e5e47c
Vladislav Dektiarev <eclipter97@gmail.com>
2018-04-03 23:39:26 +0300

    Add fans searching service

---
+++ src/main/java/by/bsuir/bigdata/service/FansSearchingService.java
@@ -0,0 +1,85 @@
+package by.bsuir.bigdata.service;
+
+import by.bsuir.bigdata.aggregation.service.HadoopAggregationService;
+import by.bsuir.bigdata.exception.FansSearchingException;
+import me.postaddict.instagram.scraper.Instagram;
+import me.postaddict.instagram.scraper.model.Account;
+import me.postaddict.instagram.scraper.model.Media;
+import me.postaddict.instagram.scraper.model.PageObject;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class FansSearchingService {
+
+    @Autowired
+    private OkHttpClient okHttpClient;
+
+    @Autowired
+    private HadoopAggregationService hadoopAggregationService;
+
+    @Value("${pages.count}")
+    private Integer pagesCount;
+
+    @Value("${likes.files.directory}")
+    private String filesDirectory;
+
+    public Map<String, Integer> searchFans(String accountName)
+    {
+        Instagram instagram = new Instagram(okHttpClient);
+
+        try {
+
+            PageObject<Media> medias = instagram.getMedias(accountName, pagesCount);
+
+            medias.getNodes().forEach(media ->
+            {
+                try {
+
+                    PageObject<Account> likes = instagram.getMediaLikes(media.getShortcode(), pagesCount);
+
+                    if(!Files.exists(Paths.get(filesDirectory + accountName)))
+                    {
+                        Files.createDirectories(Paths.get(filesDirectory + accountName));
+                    }
+
+                    Files.write(Paths.get(filesDirectory + accountName + "/" + media.getId()),
+                            likes.getNodes().stream().map(Account::getUsername).collect(Collectors.toList()),
+                            StandardOpenOption.CREATE);
+
+                } catch (IOException e) {
+                    throw new FansSearchingException(e);
+                }
+            });
+
+            hadoopAggregationService.aggregate(filesDirectory + accountName + "/",
+                    filesDirectory + accountName + "/out");
+
+            Map<String, Integer> result = new LinkedHashMap<>();
+
+            Files.readAllLines(Files.walk(Paths.get(filesDirectory + accountName + "/out"))
+                    .findFirst().get())
+                    .stream()
+                    .collect(Collectors.toMap(line -> line.split(" ")[0],
+                            line -> Integer.parseInt(line.split(" ")[1]))).entrySet().stream()
+                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                    .forEachOrdered(x -> result.put(x.getKey(), x.getValue()));
+
+            return result;
+
+        } catch (IOException e) {
+            throw new FansSearchingException(e);
+        }
+    }
+}
